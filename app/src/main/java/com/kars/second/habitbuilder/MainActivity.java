5d3e6e177b15eebf4895aa0a5105080c3d13991d
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2019-03-15 22:05:21 +0530

    Add main view activity.

--- app/src/main/java/com/kars/second/habitbuilder/MainActivity.java
+++ app/src/main/java/com/kars/second/habitbuilder/MainActivity.java
@@ -1,5 +1,7 @@
 package com.kars.second.habitbuilder;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent = new Intent(MainActivity.this,MainViewHabit.class);
+                startActivity(mIntent);
+            }
+        },2000);
     }
 }
