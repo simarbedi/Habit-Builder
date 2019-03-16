db27fcad757beef861c6dc21c81011ec05b05941
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2019-03-16 22:04:49 +0530

    Connect login activity to main_activity.

--- app/src/main/java/com/kars/second/habitbuilder/MainActivity.java
+++ app/src/main/java/com/kars/second/habitbuilder/MainActivity.java
@@ -5,19 +5,33 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kars.second.habitbuilder.Login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser mUser = mAuth.getCurrentUser();
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mIntent = new Intent(MainActivity.this,MainViewHabit.class);
-                startActivity(mIntent);
+                if(mUser == null) {
+                    Intent mIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(mIntent);
+                }else{
+                    Intent m2Intent = new Intent(MainActivity.this,MainViewHabit.class);
+                    startActivity(m2Intent);
+                }
+                finish();
             }
         },2000);
     }
+
 }
